46f89e08c2074687ecfa46909d9ac25a1bf0b965
CoreryBlack <11421410+blackcorery@user.noreply.gitee.com>
2024-09-25 17:30:04 +0800

    初始化工程

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -79,7 +79,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     public Docket docket() {
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("苍穹外卖项目接口文档")
-                .version("2.0")
+                .version("3.0")
                 .description("苍穹外卖项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
